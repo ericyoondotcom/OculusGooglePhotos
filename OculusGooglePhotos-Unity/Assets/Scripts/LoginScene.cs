b7c7a9dbd6f1f5042015106e0e47c2980f0c13d8
Eric Yoon <emjyoon@gmail.com>
2023-03-12 00:52:04 -0700

    Start auth scene

---
+++ OculusGooglePhotos-Unity/Assets/Scripts/LoginScene.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginScene : MonoBehaviour
+{
+    public void OnLoginButtonClick()
+    {
+        Application.OpenURL("https://yoonicode.com");
+    }
+}
